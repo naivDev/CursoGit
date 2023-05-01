51e532597f7cead27dc7c49bab38d22fe9b4e5c2
Oscar Ivan <osivpe@gmail.com>
2023-05-01 16:22:40 -0600

    Initial Commit

---
+++ cursoGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cursoGit
+//
+//  Created by Oscar Ivan Pérez Salazar on 01/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
