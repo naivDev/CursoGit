39a55d8b8260ba31e29ee5fc48f16aef09cac420
Oscar Ivan <osivpe@gmail.com>
2023-05-01 16:56:55 -0600

    Segundo commit

--- cursoGit/ViewController.swift
+++ cursoGit/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the vie
         print("Hola mundo")
+        print("Hola mundo2")
     }
 
 
