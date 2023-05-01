480d1ce51f71117c669678ab151e43c5b46b02fe
Oscar Ivan <osivpe@gmail.com>
2023-05-01 16:27:28 -0600

    Primer commit

--- cursoGit/ViewController.swift
+++ cursoGit/ViewController.swift
@@ -11,7 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the vie
+        print("Hola mundo")
     }
 
 
